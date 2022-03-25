9b02ca6f02ed956056b1120e6935e084fddcde07
meltyyyyy <takeru.abe.g1@gmail.com>
2022-03-25 07:46:46 +0000

    initial commit

---
+++ utils/convert_to_feather.py
@@ -0,0 +1,12 @@
+mport pandas as pd
+
+target = [
+    'train',
+    'test',
+]
+
+extension = 'csv'
+
+for t in target:
+(pd.read_csv('./data/input/' + t + '.' + extension, encoding="utf-8"))\
+	.to_feather('./data/input/' + t + '.feather')
---
+++ utils/load_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+from logging import Formatter, FileHandler, getLogger
+
+LOG_DIR = '../logs/'
+TRANSACTION_DATA = '~/handm/data/input/transactions_train.csv'
+CUSTOMER_DATA = '~/handm/data/input/customers.csv'
+ARTICLE_DATA = '~/handm/data/input/articles.csv'
+TEST_DATA = '~/ubiquant/data/input/test.csv'
+
+
