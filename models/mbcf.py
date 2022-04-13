32fa14db5f017c01efd4ce50c49f1d7e1e5500e3
meltyyyyy <takeru.abe.g1@gmail.com>
2022-04-13 02:15:23 +0000

    check if matrix contains NaN

--- models/mbcf.py
+++ models/mbcf.py
@@ -35,10 +35,13 @@ logger.info(f'arranged tran_df shape: {tran_df.shape}')
 logger.info(f'arranged tran_df sample: \n{tran_df.head()}')
 
 tran_df = tran_df.loc[:, ['customer_id', 'article_id']]
-logger.info(f'selected tran_df: {tran_df.head()}')
+logger.info(f'selected tran_df: \n{tran_df.head()}')
 
 arti_df = arti_df.loc[:, ['article_id', 'product_type_name']]
-logger.info(f'selected arti_df: {arti_df.head()}')
+logger.info(f'selected arti_df: \n{arti_df.head()}')
 
-cust_prod_matrix = pd.concat([tran_df, arti_df], axis=1).drop(columns='article_id')
-logger.debug(f'customer product matrix: \n{cust_prod_matrix}')
+cust_prod_matrix = pd.merge(tran_df, arti_df, on='article_id', how='inner')
+logger.debug(f'customer product matrix: \n{cust_prod_matrix.head()}')
+logger.debug('check if article_id contains Nan: \n{}'.format(cust_prod_matrix[cust_prod_matrix['article_id'].isna()]))
+logger.debug('check if customer_id contains Nan: \n{}'.format(cust_prod_matrix[cust_prod_matrix['customer_id'].isna()]))
+logger.debug('check if product_type_name contains Nan: \n{}'.format(cust_prod_matrix[cust_prod_matrix['product_type_name'].isna()]))
