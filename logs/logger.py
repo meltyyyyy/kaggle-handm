8b579a6353e595c8a42f9b1e8109644b6dc03aa4
meltyyyyy <takeru.abe.g1@gmail.com>
2022-04-15 00:35:05 +0000

    create logger.py

--- logs/logger.py
+++ logs/logger.py
@@ -1,6 +1,13 @@
 import json
+from logging import config, getLogger
 
 LOG_CONF = '../configs/log.json'
 
-def setLogger(log_file):
-    log_
+
+def get_logger(module_name, log_file):
+    with open(LOG_CONF, 'r') as f:
+        log_conf = json.load(f)
+
+    log_conf["handlers"]["fileHandler"]["filename"] = log_file
+    config.dictConfig(log_conf)
+    return getLogger(module_name)
